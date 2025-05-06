39f204b6f42f1d5cd992d642cee0fe0adaa097b0
towienko <01169218@pw.edu.pl>
2025-05-06 23:31:33 +0200

    Add more visit statuses

--- visit_manager/postgres_utils/models/misc.py
+++ visit_manager/postgres_utils/models/misc.py
@@ -2,8 +2,21 @@ import enum
 
 
 class VisitStatus(str, enum.Enum):
+    """
+    pending: Visit is pending and waiting for vendor confirmation
+    rejected: Vendor rejected the visit
+    confirmed: Visit is confirmed by the vendor, the visit has not started yet
+    in_progress: Visit is in progress
+    completed: Visit is completed
+    client_cancelled: Client cancelled the visit, no matter the previous status
+    vendor_cancelled: Vendor cancelled the visit, no matter the previous status
+    """
+
     __tablename__ = "visit_status"
     pending = "pending"
+    vendor_rejected = "vendor_rejected"
+    client_rejected = "client_rejected"
+    confirmed = "confirmed"
     in_progress = "in_progress"
     completed = "completed"
     cancelled = "cancelled"
