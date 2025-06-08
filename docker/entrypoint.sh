b25c869cebf33fe7c65402a911087838eecf0e6b
towienko <01169218@pw.edu.pl>
2025-06-08 20:18:05 +0200

    Add SSL support

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,10 @@
 if [ "$1" = "debug" ]; then
     poetry run uvicorn --reload visit_manager.app.main:app --port 8082 --host 0.0.0.0
 elif [ "$1" = "run" ]; then
-    uvicorn --workers "${UVICORN_WORKERS:=4}" visit_manager.app.main:app --port 8082
+    uvicorn --workers "${UVICORN_WORKERS:=4}" visit_manager.app.main:app --port 8082 --host 0.0.0.0
+elif [ "$1" = "run-https" ]; then
+    set -u
+    uvicorn --workers "${UVICORN_WORKERS:=4}" visit_manager.app.main:app --port 8082 --host 0.0.0.0 --ssl-keyfile "$SSL_KEYFILE" --ssl-certfile "$SSL_CERTFILE"
 else
     echo "Usage:
     $0 (run|debug)"
